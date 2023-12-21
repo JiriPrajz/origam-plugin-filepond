c3054b4cb484f4327e99eafbfafc150b088c7552
jiri.prajz <jiri.prajz@advantages.cz>
2023-12-21 13:58:37 +0100

    changed imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import S from './styles.module.scss';
 import { observable } from "mobx";
 import React, { useState } from "react";
-import {
-  ILocalization,
-  ILocalizer,
-  IScreenPlugin,
-  IScreenPluginData,
-} from "@origam/plugins";
+import { IScreenPlugin } from "plugins/interfaces/IScreenPlugin";
+import { IScreenPluginData } from "plugins/interfaces/IScreenPluginData";
+import { ILocalization } from "plugins/interfaces/ILocalization";
+import { ILocalizer } from "plugins/interfaces/ILocalizer";
+
 import { FilePond,registerPlugin } from "react-filepond";
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
